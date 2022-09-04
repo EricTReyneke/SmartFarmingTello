939e8f66faac497b55712dac6a869a08f2aa7266
EricTReyneke <eric2001reyneke@gmail.com>
2022-09-04 14:38:42 +0200

    go_xyz_speed
    Adding some functions that we think will be able to make the drone go in a 2 by 2m square.

--- TelloDroneCode/main.py
+++ TelloDroneCode/main.py
@@ -1,9 +1,17 @@
-from djitellopy import Tello, Tello, BackgroundFrameRead
-import threading
-threads_initialized = False
+from djitellopy import Tello
+# from threading import Threads
+# threads_initialized = False
 
 tello = Tello()
 tello.connect()
 
+distance = tello.get_distance_tof()
+
 tello.takeoff()
+tello.go_xyz_speed(90, 70, 200, 20)
+tello.go_xyz_speed(90, 70, 200, 20)
+tello.go_xyz_speed(90, 70, 200, 20)
+tello.go_xyz_speed(90, 70, 200, 20)
+# tello.go_xyz_speed_mid(100, 100, 70, 10)
 tello.land()
+tello.get_flight_time()
