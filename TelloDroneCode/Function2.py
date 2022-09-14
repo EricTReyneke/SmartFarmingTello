7752df491b2e86b4af0e68f0a7f30f8f6112988b
ChristianZac <62380071+ChristianZac@users.noreply.github.com>
2022-09-14 18:48:53 +0200

    Update Function2.py

--- TelloDroneCode/Function2.py
+++ TelloDroneCode/Function2.py
@@ -10,7 +10,17 @@ tello.takeoff()
 x = 4
 
 for i in x:
-    def curve_xyz_speed(self, x1: int, y1: int, z1: int, x2: int, y2: int, z2: int, speed: int)
+    def curve_xyz_speed(self, x1: int, y1: int, z1: int, x2: int, y2: int, z2: int, speed: int):
+        x1: 90
+        x2: 100
+        y1: 70
+        y2: 80
+        z1: 200
+        z2: 210
+        speed: 20
+
+        cmd = 'curve {} {} {} {} {} {} {}'.format(x1, y1, z1, x2, y2, z2, speed)
+        self.send_control_command(cmd)
 
 
 tello.land()
