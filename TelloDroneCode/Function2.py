2a060e8fe5825d63d7b0847ab16a1a6ea47b1371
ChristianZac <62380071+ChristianZac@users.noreply.github.com>
2022-09-14 18:45:06 +0200

    2nd Idea

---
+++ TelloDroneCode/Function2.py
@@ -0,0 +1,16 @@
+from djitellopy import Tello
+
+tello = Tello()
+tello.connect
+
+distance = Tello.get_distance_tof()
+
+tello.takeoff()
+
+x = 4
+
+for i in x:
+    def curve_xyz_speed(self, x1: int, y1: int, z1: int, x2: int, y2: int, z2: int, speed: int)
+
+
+tello.land()
