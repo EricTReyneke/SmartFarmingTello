28d2d84fa4a70a0e17d9ecb4a1b8592da57fd837
ChristianZac <62380071+ChristianZac@users.noreply.github.com>
2022-11-03 12:21:16 +0200

    Eric Se Code

--- TelloDroneCode/main.py
+++ TelloDroneCode/main.py
@@ -1,55 +1,57 @@
-# from djitellopy import Tello
-# # from threading import Threads
-# # threads_initialized = False
-# import time
-#
-# tello = Tello()
-# tello.connect()
-#
-# def TellFly():
-#     # distance = tello.get_distance_tof()
-#
-#     print(tello.get_battery())
-#
-#     tello.takeoff()
-#     time.sleep(5)
-#     # tello.go_xyz_speed(90, 70, 200, 20)
-#     # tello.go_xyz_speed(90, 70, 200, 20)
-#     # tello.go_xyz_speed(90, 70, 200, 20)
-#     # tello.go_xyz_speed(90, 70, 200, 20)
-#     # tello.go_xyz_speed_mid(100, 100, 70, 10)
-#     tello.send_rc_control(10, 0, 0, 0)
-#     # time.sleep(5)
-#     tello.send_rc_control(0, 0, 0, 0)
-#     time.sleep(5)
-#     tello.send_rc_control(-10, 0, 0, 0)
-#     # time.sleep(5)
-#     tello.send_rc_control(0, -0, 0, 0)
-#     time.sleep(5)
-#     tello.land()
-#     tello.get_flight_time()
-
-
+from djitellopy import Tello
+# from threading import Threads
+# threads_initialized = False
+import time
 import mysql
 import mysql.connector
-from mysql.connector import Error
+
+
+tello = Tello()
+tello.connect()
+
+def TellFly():
+    # distance = tello.get_distance_tof()
+
+    print(tello.get_battery())
+
+    tello.takeoff()
+    time.sleep(5)
+    # tello.go_xyz_speed(90, 70, 200, 20)
+    # tello.go_xyz_speed(90, 70, 200, 20)
+    # tello.go_xyz_speed(90, 70, 200, 20)
+    # tello.go_xyz_speed(90, 70, 200, 20)
+    # tello.go_xyz_speed_mid(100, 100, 70, 10)
+    tello.send_rc_control(10, 0, 0, 0)
+    # time.sleep(5)
+    tello.send_rc_control(0, 0, 0, 0)
+    time.sleep(5)
+    tello.send_rc_control(-10, 0, 0, 0)
+    # time.sleep(5)
+    tello.send_rc_control(0, -0, 0, 0)
+    time.sleep(5)
+    tello.land()
+    tello.get_flight_time()
 
 def Connection():
     connection = mysql.connector.connect(host='localhost',
-                                         database='prj381_group20',
+                                         database='prg381_group20',
                                          user='root',
                                          password='Foefiefa@C1')
 
-    sql_select_Query = "select Status from Route"
+    sql_select_Query = "SELECT * FROM route"
     cursor = connection.cursor()
     cursor.execute(sql_select_Query)
     # get all records
     records = cursor.fetchall()
     print("Total number of rows in table: ", cursor.rowcount)
 
-    checkFly = records
+    for row in records:
+        State = row[1]
 
-    for rows in records:
-        checkFly = rows["Status"]
-    print(checkFly)
+    print(State)
+    return State
+
 Connection()
+
+if Connection() == 1:
+    TellFly()
